c6cad43e6c2b4db35e5a7bb6b6ba256e49077c20
rizziie <rizqyauliaoncampus@gmail.com>
2026-06-15 08:00:30 +0700

    SIAP EAS

--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,104 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DosenController ;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
+use App\Http\Controllers\keranjangController;
+use App\Http\Controllers\snackController;
+use App\Http\Controllers\nilaiController;
+use App\Http\Controllers\SiswaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('halo', function () {
-	return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+	return "<h1>Halo, Selamat datang</h1> di tutorial laravel <i>www.malasngoding.com</i>";
+});
+
+Route::get('blog', function () {
+    return view('blog');
 });
 
 Route::get('pert5', function () {
     return view('pertemuan5');
 });
 
-Route::get('/pegawai','PegawaiDBController@index');
+Route::get('dosen', [DosenController::class, 'index']);
 
-Route::get('/news', function () {
-    return view('news');
+Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('pert1', function () {
+    return view('pertemuan1');
 });
 
-Route::get('/navspert3', function () {
-    return view('homepage');
+Route::get('pert2part1', function () {
+    return view('pertemuan2part1');
 });
 
-Route::get('/pertemuan4', function () {
+Route::get('pert2part2', function () {
+    return view('pertemuan2part2');
+});
+
+Route::get('pert3part1', function () {
+    return view('pertemuan3part1');
+});
+
+Route::get('pert3part2', function () {
+    return view('pertemuan3part2');
+});
+
+Route::get('tugaspert3', function () {
+    return view('tugasPertemuan3');
+});
+
+Route::get('pert4', function () {
     return view('pertemuan4');
 });
 
-Route::get('/pertemuan5', function () {
-    return view('pertemuan5');
+Route::get('pert5part2', function () {
+    return view('pertemuan5part2');
 });
 
-Route::get('/linktree', function () {
-    return view('linktreefinal');
+Route::get('tugaspert5', function () {
+    return view('tugasPertemuan5');
 });
 
+//crud tabel pegawai
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawaitambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawaistore', [PegawaiDBController::class, 'store']);
+Route::get('/pegawaiedit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawaiupdate', [PegawaiDBController::class, 'update']);
+Route::get('/pegawaihapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawaicari', [PegawaiDBController::class, 'cari']);
 
-Route::get('dosen', [DosenController::class, 'index']);
-Route::get('biodata', [DosenController::class, 'biodata']);
+//crud tabel keranjangbelanja
+Route::get('/keranjangbelanja', [keranjangController::class, 'index_keranjang']);
+Route::get('/keranjangbeli/{id}', [keranjangController::class, 'beli']);
+Route::post('/keranjangstore', [keranjangController::class, 'storeKeranjang']);
+Route::get('/keranjangbatal/{id}', [keranjangController::class, 'batal']);
 
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+//crud tabel beras
+Route::get('/beras', [berasController::class, 'indexBeras']);
+Route::get('/beras_tambah', [berasController::class, 'tambahBeras']);
+Route::post('/beras_store', [berasController::class, 'storeBeras']);
+Route::get('/beras_edit/{id}', [berasController::class, 'editBeras']);
+Route::post('/beras_update', [berasController::class, 'updateBeras']);
+Route::get('/beras_hapus/{id}', [berasController::class, 'hapusBeras']);
+Route::get('/beras_cari', [berasController::class, 'cariBeras']);
+
+//crud tabel nilaikuliah
+Route::get('/nilaikuliah', [nilaiController::class, 'indexNilai']);
+Route::get('/nilai_tambah', [nilaiController::class, 'tambahNilai']);
+Route::post('/nilai_store', [nilaiController::class, 'storeNilai']);
+
+//crud tabel siswa
+Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
+Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
+Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+Route::get('/siswa/{nrp}/edit', [SiswaController::class, 'edit'])->name('siswa.edit');
+Route::put('/siswa/{nrp}', [SiswaController::class, 'update'])->name('siswa.update');
+Route::delete('/siswa/{nrp}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
